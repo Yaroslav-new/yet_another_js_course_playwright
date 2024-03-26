fc79dea9667bb75ef1d773c1c6e29ec7b9e781b6
Yaroslav-new <yaroslav.berezhnyi.nl@gmail.com>
2024-03-26 22:52:40 +0100

    fixed comment and second test

--- tests/my-first-test.spec.js
+++ tests/my-first-test.spec.js
@@ -1,13 +1,11 @@
 import { test, expect } from '@playwright/test';
-import { exec } from 'child_process';
 
 const standardUser = "standard_user";
 const password = "secret_sauce";
-const url = 'https://www.saucedemo.com/';
 
 test.describe('Set of two tests', () => {
   test.beforeEach(async ({ page }) => {
-    await page.goto(url);
+    await page.goto('');
     await page.locator('#user-name').fill(standardUser);
     await page.locator('[data-test="password"]').fill(password);
     await page.locator('[data-test="login-button"]').click();
@@ -21,6 +19,11 @@ test.describe('Set of two tests', () => {
   });
 
   test('Add product to the cart', async ({ page }) => {
-
+    await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+    await page.locator('.shopping_cart_link').click();
+    await expect(page.locator('.shopping_cart_badge')).toHaveCount(1);
+    await expect(page.locator('.inventory_item_name')).toHaveText('Sauce Labs Backpack');
+    await page.locator('[data-test="remove-sauce-labs-backpack"]').click();
+    await expect(page.locator(".cart_list > .removed_cart_item"));
   });
 });
