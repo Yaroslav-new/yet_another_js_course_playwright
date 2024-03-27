502487584760171c9cc2581daf663ecd936b8025
Yaroslav-new <141754306+Yaroslav-new@users.noreply.github.com>
2024-03-27 21:06:16 +0100

    Home task is 1  (#1)
    * home task 1: test 1 is done
    
    * fixed comment and second test
    
    * Fixed test

---
+++ tests/my-first-test.spec.js
@@ -0,0 +1,29 @@
+import { test, expect } from '@playwright/test';
+
+const standardUser = "standard_user";
+const password = "secret_sauce";
+
+test.describe('Set of two tests', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('');
+    await page.locator('#user-name').fill(standardUser);
+    await page.locator('[data-test="password"]').fill(password);
+    await page.locator('[data-test="login-button"]').click();
+  });
+
+  test('Perform Login', async ({ page }) => {
+    await expect (page.locator('.title')).toBeVisible();
+    await expect (page.locator('.shopping_cart_link')).toBeVisible();
+    const productsItems = await (page.locator('.inventory_item')).count()
+    expect(productsItems).toBeGreaterThan(1);
+  });
+
+  test('Add product to the cart', async ({ page }) => {
+    await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+    await page.locator('.shopping_cart_link').click();
+    await expect(page.locator('.shopping_cart_badge')).toHaveCount(1);
+    await expect(page.locator('.inventory_item_name')).toHaveText('Sauce Labs Backpack');
+    await page.locator('[data-test="remove-sauce-labs-backpack"]').click();
+    await expect(page.locator(".cart_item_label")).toBeHidden();
+  });
+});
